03bd90215fad7fcb3d5e9264cd868db06376b48e
Jacob <jacobpierce@uchicago.edu>
2018-06-21 19:00:32 -0500

    about to fix commits

---
+++ jutils/basis_function.py
@@ -0,0 +1,23 @@
+# # class providing easy use of finite numbers of compactly supported basis
+# # functions. 
+
+# import numpy as np 
+
+
+# # these basis functinos are optimized for integration. the support should be checked
+# # when performing a numerical integration, and there is no check here on whether
+# # entries belong to the support or not. function takes as input ( i, x ) where
+# # i is the basis function number and x is the value to be evaluated.
+
+# class basis_functions_support_translated( object ) :
+
+#     def __init__( self, function, supports ) :
+
+#          self.samefunc = samefunc
+
+#     def eval( self, x ) :
+
+        
+
+            
+        
---
+++ jutils/prototype/basis_functions_proto.py
@@ -0,0 +1,20 @@
+
+import numpy as np 
+
+
+# class basis_functions( object ) :
+
+#     def __init__( self ) :
+
+        
+        
+x = [0]*5
+
+for i in range(5) :
+
+    def func( z ) :
+        return i
+
+    x[i] = func
+
+print( x[0](5), x[1](4) ) 
